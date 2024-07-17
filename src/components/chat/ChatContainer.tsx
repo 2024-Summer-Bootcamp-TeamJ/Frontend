6b218c31da7ff95a31364f1d60692a54977fd922
Shin-Sujin <tnwls5621@tukorea.ac.kr>
2024-07-18 01:14:56 +0900

    debug : 깃 충돌 에러 해결

--- src/components/chat/ChatContainer.tsx
+++ src/components/chat/ChatContainer.tsx
@@ -1,15 +1,14 @@
-
-import React, { useEffect, useRef } from 'react';
-import MentorChatBubble from './MentorChatBubble';
-import MyChatBubble from './MyChatBubble';
-import ChatInput from './ChatInput';
-
+import React, { useEffect, useRef, useState, CSSProperties } from "react";
+import MentorChatBubble from "./MentorChatBubble";
+import MyChatBubble from "./MyChatBubble";
+import ChatInput from "./ChatInput";
 
 interface ChatContainerProps {
   mentorBgColor: string;
   myBgColor: string;
-
   scrollbarColor: string; // 스크롤바 색상을 위한 새로운 prop 추가
+  messages: string[];
+  onSendMessage: (message: string) => void;
 }
 
 // CSSProperties 타입 확장
@@ -20,19 +19,11 @@ interface CustomCSSProperties extends CSSProperties {
 const ChatContainer: React.FC<ChatContainerProps> = ({
   mentorBgColor,
   myBgColor,
-  scrollbarColor,  messages, onSendMessage // 스크롤바 색상을 위한 새로운 prop 추가
+  scrollbarColor,
+  messages,
+  onSendMessage,
 }) => {
-  const [messages, setMessages] = useState<string[]>([
-    "어떤 상담이 필요하냐곰",
-    "백곰원님 제가 일식당을 차리려고 하는데 메뉴를 50개 정도 하고싶어요. 괜찮을까요?",
-    "미쳤냐곰 말도 안되는 소리 하지 말라곰 그러다 망한다곰",
-    "허거덩 진짜요? 우짜죠 이미 개업했는디",
-  ]);
-
-
-  messages: string[];
-  onSendMessage: (message: string) => void;
-}
+  const [messageList, setMessageList] = useState<string[]>(messages);
 
   const messagesEndRef = useRef<HTMLDivElement | null>(null);
 
@@ -44,22 +35,26 @@ const ChatContainer: React.FC<ChatContainerProps> = ({
 
   useEffect(() => {
     scrollToBottom();
-  }, [messages]);
+  }, [messageList]);
 
   return (
     <div className="relative w-full max-w-3xl mx-auto">
-
       <div className="relative bg-white bg-opacity-80 rounded-3xl p-8 w-[60vh] h-[80vh] flex flex-col justify-between">
         <div className="flex-grow overflow-y-scroll font-syndinaroo text-red-500 space-y-4">
-          {messages.map((message, index) => (
+          {messageList.map((message, index) => (
             <div key={index}>
-              {message.startsWith('Client:') ? (
-                <MyChatBubble chatMessage={message.replace('Client: ', '')} bgColor={myBgColor} />
+              {message.startsWith("Client:") ? (
+                <MyChatBubble
+                  chatMessage={message.replace("Client: ", "")}
+                  bgColor={myBgColor}
+                />
               ) : (
-                <MentorChatBubble chatMessage={message} bgColor={mentorBgColor} />
+                <MentorChatBubble
+                  chatMessage={message}
+                  bgColor={mentorBgColor}
+                />
               )}
             </div>
-
           ))}
           <div ref={messagesEndRef} />
         </div>
