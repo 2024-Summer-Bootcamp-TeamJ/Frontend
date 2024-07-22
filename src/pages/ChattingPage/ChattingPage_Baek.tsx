195eb9850b00867c075ea2efec5305326bcececc
Shin-Sujin <tnwls5621@tukorea.ac.kr>
2024-07-22 11:43:18 +0900

    design : 백 채팅 페이지 반응형 완성, chatcontainer수정함

--- src/pages/ChattingPage/ChattingPage_Baek.tsx
+++ src/pages/ChattingPage/ChattingPage_Baek.tsx
@@ -126,57 +126,45 @@ const ChattingPageBaek: React.FC = () => {
             onClick={endChatAndGoToPrescription}
           />
         </div>
-        <div
-          className="relative min-h-screen bg-cover bg-center flex items-center justify-center"
-          style={{ backgroundImage: `url(${backgroundBaek})` }}
-        >
-          <div className="absolute top-4 right-4">
-            <Button
-              text="대화 종료하기"
-              color="bg-green-700 w-32 pt-3 pb-3 font-bold"
-              onClick={endChatAndGoToPrescription}
-            />
-          </div>
-          {/* 데스크탑 버전 시작 */}{" "}
-          <div className="hidden md:block relative flex items-center justify-center w-full max-w-6xl px-2 py-4 bg-green-50 bg-opacity-90 rounded-3xl shadow-md">
-            <div className="relative flex items-center space-x-40 px-0 py-8 overflow-visible">
-              <div className=" relative ml-16 mb-12">
+        {/* 데스크탑 버전 시작 */}{" "}
+        <div className="hidden md:block relative flex items-center justify-center w-full max-w-6xl px-2 py-4 bg-green-50 bg-opacity-90 rounded-3xl shadow-md">
+          <div className="relative flex items-center space-x-40 px-0 py-8 overflow-visible">
+            <div className=" relative ml-16 mb-12">
+              <img
+                src={characterBaek}
+                alt="Baek"
+                className="w-72 ml-16 h-auto relative bounce-animation"
+                draggable="false"
+              />
+              <div className="absolute top-18 left-1/2 transform -translate-x-1/2 -translate-y-16">
                 <img
-                  src={characterBaek}
-                  alt="Baek"
-                  className="w-72 ml-16 h-auto relative bounce-animation"
+                  src={chatBubbleImage}
+                  alt="Chat Bubble"
+                  className="w-60 h-auto z-20 ml-8 -mt-4"
+                  style={{ transform: "scale(2.5)" }}
                   draggable="false"
                 />
-                <div className="absolute top-18 left-1/2 transform -translate-x-1/2 -translate-y-16">
-                  <img
-                    src={chatBubbleImage}
-                    alt="Chat Bubble"
-                    className="w-60 h-auto z-20 ml-8 -mt-4"
-                    style={{ transform: "scale(2.5)" }}
-                    draggable="false"
-                  />
-                  <div
-                    className="absolute bottom-4 -left-8 w-full h-full flex items-center justify-center"
-                    style={{ width: "180%", height: "100%" }}
+                <div
+                  className="absolute bottom-4 -left-8 w-full h-full flex items-center justify-center"
+                  style={{ width: "180%", height: "100%" }}
+                >
+                  <p
+                    className="text-center text-3xl text-dateTextColor font-syndinaroo"
+                    style={{ transform: "scale(1)" }}
                   >
-                    <p
-                      className="text-center text-3xl text-dateTextColor font-syndinaroo"
-                      style={{ transform: "scale(1)" }}
-                    >
-                      {latestServerMessage}
-                    </p>
-                  </div>
+                    {latestServerMessage}
+                  </p>
                 </div>
               </div>
-              <div className="ml-0 overflow-visible">
-                <ChatContainer
-                  mentorBgColor="bg-[#F0FDDE]"
-                  myBgColor="bg-[#DEFFB2]"
-                  scrollbarColor="#A0DA6A"
-                  messages={messages}
-                  onSendMessage={sendMessage}
-                />
-              </div>
+            </div>
+            <div className="ml-0 overflow-visible">
+              <ChatContainer
+                mentorBgColor="bg-[#F0FDDE]"
+                myBgColor="bg-[#DEFFB2]"
+                scrollbarColor="#A0DA6A"
+                messages={messages}
+                onSendMessage={sendMessage}
+              />
             </div>
           </div>
         </div>
