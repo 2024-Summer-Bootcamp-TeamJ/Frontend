3e59cf7a8f55a69d963c2558206f4b46b01e4f0f
신성우 <tlstjd1007@naver.com>
2024-07-29 17:57:48 +0900

    클라이언트 문장 분리 오류 해결

--- src/components/chat/ChatContainer.tsx
+++ src/components/chat/ChatContainer.tsx
@@ -4,7 +4,6 @@ import MyChatBubble from "./MyChatBubble";
 import ChatInput from "./ChatInput";
 import useSound from "use-sound";
 import button_pressed from "../../assets/audios/button_pressed.mp3";
-import "../../index.css";
 
 interface ChatContainerProps {
   mentorBgColor: string;
@@ -21,9 +20,17 @@ interface CustomCSSProperties extends CSSProperties {
 }
 
 const splitIntoSentences = (text: string) => {
-  return (
-    text.match(/[^\.!\?]+[\.!\?]+/g)?.map((sentence) => sentence.trim()) || []
-  );
+  const sentences = text.split(/([.!?]+)/).filter(Boolean);
+  let result = [];
+
+  for (let i = 0; i < sentences.length; i += 2) {
+    let sentence = sentences[i];
+    if (sentences[i + 1]) {
+      sentence += sentences[i + 1];
+    }
+    result.push(sentence.trim());
+  }
+  return result;
 };
 
 const ChatContainer: React.FC<ChatContainerProps> = ({
@@ -39,13 +46,16 @@ const ChatContainer: React.FC<ChatContainerProps> = ({
   const messagesEndRef = useRef<HTMLDivElement | null>(null);
   const previousMessagesRef = useRef<string[]>([]);
 
-  const scrollToBottom = () => {
-    console.log("scrollToBottom called");
 
+  const scrollToBottom = () => {
     if (messagesEndRef.current) {
       messagesEndRef.current.scrollIntoView({ behavior: "smooth" });
     }
   };
+  
+  useEffect(() => {
+    scrollToBottom();
+  }, [messageList]);
 
   // useEffect(() => {
   //   setMessageList(messages); // messages prop이 변경될 때 messageList를 업데이트
@@ -56,40 +66,39 @@ const ChatContainer: React.FC<ChatContainerProps> = ({
     const newMessages = messages.filter(
       (msg) => !previousMessagesRef.current.includes(msg)
     );
+  
     if (newMessages.length > 0) {
       let index = 0;
-      const sentences = newMessages.flatMap((msg) => splitIntoSentences(msg));
-
+      const sentences = newMessages.flatMap(msg => {
+        if (!msg.startsWith('Client:')) {
+          return splitIntoSentences(msg);
+        } else {
+          return [msg];
+        }
+      });
+  
       const interval = setInterval(() => {
         if (index < sentences.length) {
           const sentence = sentences[index];
-          if (sentence) {
-            setMessageList((prevMessages) => {
-              if (!prevMessages.includes(sentence)) {
-                return [...prevMessages, sentence];
-              }
-              return prevMessages;
-            });
+          if (sentence && !messageList.includes(sentence)) {
+            setMessageList((prevMessages) => [...prevMessages, sentence]);
             scrollToBottom();
-            index++;
           }
+          index++;
         } else {
           clearInterval(interval);
         }
       }, 1500);
-
-      previousMessagesRef.current = [
-        ...previousMessagesRef.current,
-        ...newMessages,
-      ];
+  
+      previousMessagesRef.current = [...previousMessagesRef.current, ...newMessages];
       return () => clearInterval(interval);
+    } else {
+      setMessageList(messages);
+      scrollToBottom();
     }
   }, [messages]);
+  
 
-  useEffect(() => {
-    console.log("메세지 리스트 : useEffect triggered");
-    scrollToBottom();
-  }, [messageList]);
 
   const handleSendMessage = (message: string) => {
     play(); // 메시지를 보내기 전에 효과음 재생
@@ -104,7 +113,7 @@ const ChatContainer: React.FC<ChatContainerProps> = ({
         <div className="relative hidden w-full max-w-3xl mx-auto md:block">
           <div className="relative bg-white bg-opacity-80 rounded-3xl p-8 w-[60vh] h-[80vh] flex flex-col justify-between">
             <div
-              className="flex-grow overflow-y-scroll scrollbar2 text-red-500 font-syndinaroo"
+              className="flex-grow overflow-y-scroll text-red-500 font-syndinaroo"
               style={
                 { "--scrollbar-color": scrollbarColor } as CustomCSSProperties
               }
@@ -112,6 +121,7 @@ const ChatContainer: React.FC<ChatContainerProps> = ({
               {messageList.map((message, index) => (
                 <div key={index} className="mb-4">
                   {" "}
+                  {/* 간격을 위한 클래스 추가 */}
                   {message.startsWith("Client:") ? (
                     <MyChatBubble
                       chatMessage={message.replace("Client: ", "")}
@@ -137,7 +147,7 @@ const ChatContainer: React.FC<ChatContainerProps> = ({
         <div className="block md:hidden relative items-center overflow-visible w-full max-w-md h-[70vh] p-2">
           <div className="relative flex flex-col w-full h-full p-2 bg-white shadow-lg bg-opacity-80 rounded-3xl">
             <div
-              className="flex-grow overflow-y-scroll scrollbar2 text-red-500 font-syndinaroo"
+              className="flex-grow overflow-y-scroll text-red-500 font-syndinaroo"
               style={
                 { "--scrollbar-color": scrollbarColor } as CustomCSSProperties
               }
